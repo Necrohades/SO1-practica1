87e314c93343c45a8af6a74b03f6782efd01a155
Necrohades <tfuentes2004@gmail.com>
2024-10-08 17:50:15 +0200

    Compactar incrementos de punteros

--- src/my_lib.c
+++ src/my_lib.c
@@ -3,25 +3,19 @@
 
 size_t my_strlen(const char *str) {
     size_t len = 0;
-
     while (*str++) {  // check if the content is 0 and increment the pointer to the next char
         len++;
     }
-    
     return len;
 }
 
 //Función que copia en la posición asociada al puntero dest, el contenido al que apunta src
 char* my_strcpy(char* dest, const char* src){
     char* tmp = dest;
-
-    while(*src != '\0'){ //Mientras el carácter al que apunte src no sea el carácter nulo
-        *dest = *src; //Copia el contenido de src a dest
-        src++;
-        dest++;
+    while (*src != '\0') { //Mientras el carácter al que apunte src no sea el carácter nulo
+        *dest++ = *src++; //Copia el contenido de src a dest
     }
     *dest = '\0'; 
-    
     return tmp;
 }
 
@@ -32,15 +26,12 @@ char *my_strncpy(char *dest, const char *src, size_t n){
     int srclen = strlen(src);
     int dstlen = strlen(dest);
 
-    for(int k = 0; k < n; k++){
-     if(k <= srclen){
-        *dest = *src;
-        src++;
-        dest++;
-     } else if(k < dstlen){
-        *dest = '\0';  
-        dest++;
-     }
+    for (int k = 0; k < n; k++){
+        if (k <= srclen) {
+            *dest++ = *src++;
+        } else if (k < dstlen) {
+            *dest++ = '\0';
+        }
     }
     return tmp; 
 }
