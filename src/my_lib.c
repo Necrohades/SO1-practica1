6203d57dfd20065e8adc0597b3888bc56a3cccd8
Necrohades <tfuentes2004@gmail.com>
2024-10-08 18:10:15 +0200

    Añadir strcat

--- src/my_lib.c
+++ src/my_lib.c
@@ -10,8 +10,8 @@ size_t my_strlen(const char *str) {
 }
 
 //Función que copia en la posición asociada al puntero dest, el contenido al que apunta src
-char* my_strcpy(char* dest, const char* src){
-    char* tmp = dest;
+char *my_strcpy(char *dest, const char *src){
+    char *tmp = dest;
     while (*src != '\0') { //Mientras el carácter al que apunte src no sea el carácter nulo
         *dest++ = *src++; //Copia el contenido de src a dest
     }
@@ -22,7 +22,7 @@ char* my_strcpy(char* dest, const char* src){
 //Función que copia los n carácteres del string al que apunta src en la posición a la que apunta dest
 //en caso de n > longitud del string de src, se añaden carácteres nulos al contenido de dest
 char *my_strncpy(char *dest, const char *src, size_t n){
-    char* tmp = dest;
+    char *tmp = dest;
     int srclen = strlen(src);
     int dstlen = strlen(dest);
 
@@ -34,4 +34,15 @@ char *my_strncpy(char *dest, const char *src, size_t n){
         }
     }
     return tmp; 
+}
+
+char *my_strcat(char *dest, const char *src) {
+    char *tmp = dest;
+    while(*tmp) tmp++;  // incrementa tmp hasta que su contenido es nulo
+    
+    do {
+        *tmp++ = *src;
+    } while (*src++);
+
+    return dest;
 }
