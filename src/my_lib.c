ae813ace1f615fe0c33799eb1c91a3789b44ee25
Necrohades <tfuentes2004@gmail.com>
2024-10-08 17:45:04 +0200

    Implementación de strcpy y strncpy de Jaume

--- src/my_lib.c
+++ src/my_lib.c
@@ -10,3 +10,38 @@ size_t my_strlen(const char *str) {
     
     return len;
 }
+#include "my_lib.h"
+
+//Función que copia en la posición asociada al puntero dest, el contenido al que apunta src
+char* my_strcpy(char* dest, const char* src){
+    char* tmp = dest;
+
+    while(*src != '\0'){ //Mientras el carácter al que apunte src no sea el carácter nulo
+        *dest = *src; //Copia el contenido de src a dest
+        src++;
+        dest++;
+    }
+    *dest = '\0'; 
+    
+    return tmp;
+}
+
+//Función que copia los n carácteres del string al que apunta src en la posición a la que apunta dest
+//en caso de n > longitud del string de src, se añaden carácteres nulos al contenido de dest
+char *my_strncpy(char *dest, const char *src, size_t n){
+    char* tmp = dest;
+    int srclen = strlen(src);
+    int dstlen = strlen(dest);
+
+    for(int k = 0; k < n; k++){
+     if(k <= srclen){
+        *dest = *src;
+        src++;
+        dest++;
+     } else if(k < dstlen){
+        *dest = '\0';  
+        dest++;
+     }
+    }
+    return tmp; 
+}
