4d2ecb0716c9bba6f6f9ce62547acc0b990f254e
Necrohades <tfuentes2004@gmail.com>
2024-10-24 23:09:51 +0200

    Update my_lib.c

--- src/my_lib.c
+++ src/my_lib.c
@@ -1,25 +1,35 @@
+/**************************************************************************
+* FILENAME: my_lib.c
+* DESCRIPTION: Libreria de funciones relativas a los retos 1 y 2 de la P1
+* AUTHOR: Toni Fuentes, Sebastián Trujillo, Jaume Galmés [STTF]
+* DATE: 24/10/2024
+**************************************************************************/
+
 #include "my_lib.h"
 
 size_t my_strlen(const char *str) {
     size_t len = 0;
-    while (*str++) {  // check if the content is 0 and increment the pointer to the next char
+    // Comprueba si el contenido es 0 e incrementa el puntero al siguiente char
+    while (*str++) {  
         len++;
     }
     return len;
 }
 
-//Función que copia en la posición asociada al puntero dest, el contenido al que apunta src
+// Función que copia en la posición asociada al puntero dest, el contenido al que apunta src
 char *my_strcpy(char *dest, const char *src) {
     char *tmp = dest;
-    while (*src != '\0') { //Mientras el carácter al que apunte src no sea el carácter nulo
-        *dest++ = *src++; //Copia el contenido de src a dest
+    while (*src != '\0') { // Mientras el carácter al que apunte src no sea el carácter nulo
+        *dest++ = *src++; // Copia el contenido de src a dest
     }
     *dest = '\0'; 
     return tmp;
 }
 
-//Función que copia los n carácteres del string al que apunta src en la posición a la que apunta dest
-//en caso de n > longitud del string de src, se añaden carácteres nulos al contenido de dest
+/*
+* Función que copia los n carácteres del string al que apunta src en la pos. a la que apunta dest
+* en caso de n > longitud del string de src, se añaden carácteres nulos al contenido de dest
+*/
 char *my_strncpy(char *dest, const char *src, size_t n) {
     char *tmp = dest;
     int srclen = strlen(src);
@@ -35,6 +45,10 @@ char *my_strncpy(char *dest, const char *src, size_t n) {
     return tmp; 
 }
 
+/*
+* Función que concatena el string apuntado por src  
+* con el string apuntado por dest.
+*/
 char *my_strcat(char *dest, const char *src) {
     char *tmp = dest;
     while(*tmp) tmp++;  // incrementa tmp hasta que su contenido es nulo
@@ -46,11 +60,13 @@ char *my_strcat(char *dest, const char *src) {
     return dest;
 }
 
-// Función que compara dos cadenas de caracteres, str1 y str2
-// Devuelve un valor entero:
-// < 0 si str1 es menor que str2
-// > 0 si str1 es mayor que str2
-// = 0 si ambas cadenas son iguales
+/*
+* Función que compara dos cadenas de caracteres, str1 y str2
+* Devuelve un valor entero:
+* < 0 si str1 es menor que str2
+* > 0 si str1 es mayor que str2
+* = 0 si ambas cadenas son iguales
+*/
 int my_strcmp(const char *str1, const char *str2) {
     // Mientras ambos caracteres sean iguales y no sea el final de ninguna cadena ('\0')
     while (*str1 && (*str1 == *str2)) {
@@ -63,8 +79,10 @@ int my_strcmp(const char *str1, const char *str2) {
     return *(unsigned char *)str1 - *(unsigned char *)str2;
 }
 
-// Función que busca la primera ocurrencia del carácter c en la cadena str
-// Devuelve un puntero al carácter encontrado o NULL si no se encuentra
+/* 
+* Función que busca la primera ocurrencia del carácter c en la cadena str
+* Devuelve un puntero al carácter encontrado o NULL si no se encuentra
+*/
 char *my_strchr(const char *str, int c) {
     // Recorre la cadena carácter a carácter
     while (*str != '\0') {
@@ -79,9 +97,11 @@ char *my_strchr(const char *str, int c) {
     return NULL;
 }
 
-//structuras para gestor de pila
 
-//declaraciones funciones gestor de pila
+/*
+* Inicializa un nuevo stack, este recibe un entero con el tamaño de los datos
+* a los que apuntará cada uno de los nodos.
+*/
 struct my_stack *my_stack_init(int size) {
     if (size <= 0) return NULL;
 
@@ -93,22 +113,19 @@ struct my_stack *my_stack_init(int size) {
     return stack;
 }
 
+/*
+* Función que añade un nuevo nodo al stack pasado por parámetro,
+* A dicho nodo se le asigna el puntero de datos también pasado como argumento.
+*/
 int my_stack_push(struct my_stack *stack, void *data) {
     if (!stack || stack->size <= 0 || !data) return EXIT_FAILURE;
 
     struct my_stack_node *new_node = (struct my_stack_node *)malloc(sizeof(struct my_stack_node));
     if (!new_node) return EXIT_FAILURE;
 
-    new_node->data = malloc(stack->size);
-    if (!new_node->data) {
-        free(new_node);
-        return EXIT_FAILURE;
-    }
-
-    memcpy(new_node->data, data, stack->size);
+    new_node->data = data;
     new_node->next = stack->top;
-    stack->top = new_node;
-    
+    stack->top = new_node;  
     return EXIT_SUCCESS;
 }
 
@@ -116,6 +133,8 @@ int my_stack_push(struct my_stack *stack, void *data) {
  * Función que elimina el nodo superior de la pila, y devuelve el puntero a los datos de ese nodo.
  */
 void *my_stack_pop(struct my_stack *stack) {
+    if(!stack->top) return NULL;
+
     struct my_stack_node top_node = *stack->top;
     free(stack->top);
     stack->top = top_node.next;
@@ -155,29 +174,104 @@ int my_stack_purge(struct my_stack *stack) {
     return freed_bytes;
 }
 
-struct my_stack *my_stack_read(char *filename);
 
+/*
+* Función que escribe en un fichero un stack pasado por parámetro,
+* el fichero sólo contiene el tamaño de los datos y los datos en si.
+*/
 int my_stack_write(struct my_stack *stack,  char *filename) {
-    if (!stack || !filename) return EXIT_FAILURE;
+    if (!stack || !filename) return -1;
 
     int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (fd == -1) return EXIT_FAILURE;
+    if (fd == -1){
+        perror("Error");
+        return -1;
+    } 
 
-    // Escribir el tamaño de los datos
+    // Escribe el tamaño de los datos
     if (write(fd, &stack->size, sizeof(int)) != sizeof(int)) {
-        close(fd);
-        return EXIT_FAILURE;
+        if(close(fd) == -1) perror("Error"); 
+        perror("Error");
+        return -1;
     }
 
+    struct my_stack *tmpStack = my_stack_init(stack->size);
+
+    // Vuelca en un stack auxiliar los nodos del stack para invertir su orden
     struct my_stack_node *current = stack->top;
     while (current) {
-        if (write(fd, current->data, stack->size) != stack->size) {
-            close(fd);
-            return EXIT_FAILURE;
-        }
+        my_stack_push(tmpStack, current->data);
         current = current->next;
     }
 
-    close(fd);
-    return EXIT_SUCCESS;
+    int count = 0;
+    current = tmpStack->top;
+    while (current) {
+        if (write(fd, current->data, stack->size) != stack->size) {
+            if(close(fd) == -1) perror("Error"); 
+            perror("Error");
+            return -1;
+        }
+        current = current->next;
+        count++;   
+    }
+
+    if(close(fd) == -1) perror("Error"); 
+    return count;
 }
+
+/*
+* Función que lee y reconstruye un stack que haya sido almacenado
+* previamente en un fichero mediante la función my_stack_write(),
+* el fichero sólo contiene el tamaño de los datos y los datos en si.
+*/
+struct my_stack *my_stack_read(char *filename){
+    if (!filename) return NULL;    
+
+    int fd = open(filename, O_RDONLY); // Abre el archivo en modo solo lectura
+    if (fd == -1){
+        perror("Error");
+        return NULL;
+    }
+
+    // Lee los primeros 4 bytes del documento para obtener el tamaño de los datos del stack
+    int size = 0;
+    if(read(fd, &size, sizeof(int)) != sizeof(int) || size <= 0){
+        if(close(fd) == -1 || size == -1) perror("Error");
+        return NULL;
+    }
+    
+    // Inicializa un nuevo stack con el tamaño de datos obtenido
+    struct my_stack *stackRead = my_stack_init(size);
+    if (!stackRead) {
+        if(close(fd) == -1) perror("Error");
+        return NULL;
+    }
+
+    void *dataRead = malloc(stackRead->size);
+    if (!dataRead) { 
+        if(close(fd) == -1) perror("Error");
+        return NULL;
+    }
+    
+    while(read(fd, dataRead, stackRead->size) > 0){ // Mientras el archivo no llegue al final (EOF, 0)
+        my_stack_push(stackRead, dataRead); // Mete nuevos nodos en el stack con los datos leídos
+        dataRead = malloc(stackRead->size);
+        if (!dataRead) {
+            free(dataRead);
+            return NULL;
+         }
+    }
+    if(*(int*)dataRead == -1){
+        perror("Error");
+        if(close(fd) == -1) perror("Error");
+        return NULL;
+    }
+
+    free(dataRead); // Libera el espacio reservado por malloc() en la última iteración del while.
+    
+    if(close(fd) == -1) perror("Error"); 
+    return stackRead;
+}
+
+
